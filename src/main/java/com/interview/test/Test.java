5bc055130641d2d3080044d0d2d8311dadfb63c0
THINK <1302981271@qq.com>
2017-07-03 15:28:09 +0800

    min updated it just now.

--- src/main/java/com/interview/test/Test.java
+++ src/main/java/com/interview/test/Test.java
@@ -7,5 +7,12 @@ public class Test {
     //这是源代码：
     public static void main(String[] args) {
         System.out.println("閔粤在此方法中布置了作业：请各位收到后一一作答。");
+        /**
+         * 1.==与equal有何区别？ （汤宇航）
+         * 2.java语言的&与&&有何区别？（覃津传）
+         * 3.
+         */
     }
+
+
 }
