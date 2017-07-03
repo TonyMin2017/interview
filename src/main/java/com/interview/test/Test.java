d583109846a6712d26a9e478ad00a93ba1c45e79
THINK <1302981271@qq.com>
2017-07-03 15:32:56 +0800

    min updated it just now.

--- src/main/java/com/interview/test/Test.java
+++ src/main/java/com/interview/test/Test.java
@@ -1,4 +1,4 @@
-package com.interview.test;
+ackage com.interview.test;
 
 /**
  * Created by THINK on 2017/7/3.
@@ -11,7 +11,8 @@ public class Test {
          * 1.==与equal有何区别？ （汤宇航）
          * 2.java语言里&的&&有何区别？（覃津传）
          * 3.java的char型变量能否存储一个中文汉字？为什么？（毛璇）
-         * 4.
+         * 4.overload与override的区别？overload的方法 是否可以改变返回值？（覃认追）
+         * 5.内存溢出是指的什么？Java会不会内存溢出？ （黄卓燊）
          */
     }
 
