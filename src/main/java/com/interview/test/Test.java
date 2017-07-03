9ab770b07e52903f0dcf05210e2b03bcdad54664
THINK <1302981271@qq.com>
2017-07-03 15:38:16 +0800

    min updated it just now.

--- src/main/java/com/interview/test/Test.java
+++ src/main/java/com/interview/test/Test.java
@@ -13,6 +13,13 @@ public class Test {
          * 3.java的char型变量能否存储一个中文汉字？为什么？（毛璇）
          * 4.overload与override的区别？overload的方法 是否可以改变返回值？（覃认追）
          * 5.内存溢出是指的什么？Java会不会内存溢出？ （黄卓燊）
+         * 6.JSP中的动态include与静态include的区别是什么？ （应传福）
+         * 7.运行时异常与一般异常有何异同？ （舒张）
+         * 8.ArrayList与LinkedList的区别？ （黄卓燊）
+         * 9.数据库的连接池的作用与工作机制分别是什么？ （梁梦楚）
+         * 10.Spring是如何实现IOC的？如何使用AOP机制配置事务？ （黄卓燊）
+         *
+         *
          */
     }
 
