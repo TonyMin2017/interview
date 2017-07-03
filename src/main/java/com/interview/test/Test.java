24a725e0b265b8aec1772834f81f6b212380e79c
THINK <1302981271@qq.com>
2017-07-03 15:11:48 +0800

    初始化的代码。

---
+++ src/main/java/com/interview/test/Test.java
@@ -0,0 +1,11 @@
+package com.interview.test;
+
+/**
+ * Created by THINK on 2017/7/3.
+ */
+public class Test {
+    //这是源代码：
+    public static void main(String[] args) {
+        System.out.println("min 提交了原代码。");
+    }
+}
