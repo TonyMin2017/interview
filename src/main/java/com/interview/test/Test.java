592338724ba1ef88a87b1bbe33b810191508694c
yuhang78 <648453648@qq.com>
2017-07-03 15:44:25 +0800

    1.题目答案

--- src/main/java/com/interview/test/Test.java
+++ src/main/java/com/interview/test/Test.java
@@ -1,4 +1,4 @@
-ackage com.interview.test;
+package com.interview.test;
 
 /**
  * Created by THINK on 2017/7/3.
@@ -9,6 +9,7 @@ public class Test {
         System.out.println("閔粤在此方法中布置了作业：请各位收到后一一作答。");
         /**
          * 1.==与equal有何区别？ （汤宇航）
+         * 答: ==比较的是地址,而 equal比较的是值(内容)
          * 2.java语言里&的&&有何区别？（覃津传）
          * 3.java的char型变量能否存储一个中文汉字？为什么？（毛璇）
          * 4.overload与override的区别？overload的方法 是否可以改变返回值？（覃认追）
